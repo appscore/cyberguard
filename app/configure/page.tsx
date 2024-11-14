4e8452f77e713a4b33a1d2df968f4b227c3aed35
Shubham <shubham.jain@trideca.com.au>
2024-11-14 11:39:07 +1100

    Added model provider

--- app/configure/page.tsx
+++ app/configure/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { ModelConfig } from "@/app/components/model";
+
 const navigation = [
     { name: "Dashboard", href: "#", current: true },
     { name: "Configure", href: "#", current: false },
@@ -14,6 +18,14 @@ const navigation = [
   }
   
   export default function Example() {
+
+    function handleModelConfigChange() {
+      // Fetch the app configuration again to update the state
+      // fetchIsAppConfigured().then((data) => {
+      //   setConfigured(data);
+      // });
+    }
+
     return (
       <>
         <header className="bg-white shadow">
@@ -25,7 +37,17 @@ const navigation = [
         </header>
         <main>
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            
+          <ModelConfig
+                sectionTitle="Start" //{configured ? "Update model" : "Start"}
+                sectionDescription= "Set up an AI model to start the app."
+                // {
+                //   configured
+                //     ? "Change to a different model or use another provider"
+                //     : "Set up an AI model to start the app."
+                // }
+                configured={false}
+                onConfigChange={handleModelConfigChange}
+              />
           </div>
         </main>
       </>
